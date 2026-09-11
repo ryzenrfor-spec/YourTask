dfbb9dd44bb794fb7a632d9b205edc9aa708164a
Putra Erlangga <ryzenrfor@gmail.com>
2026-09-11 17:44:38 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* YourTask Service Worker - GitHub Pages Project Site (v8 - cache-first offline) */
-const CACHE_NAME = 'yourtask-cache-v8';
+/* YourTask Service Worker - GitHub Pages Project Site (v9 - repair some bugs) */
+const CACHE_NAME = 'yourtask-cache-v9';
 const BASE = '/YourTask/';
 
 const PRECACHE_URLS = [
