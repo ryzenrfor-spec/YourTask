8509f85d41ab52959bf7e06dd403cf202ad1db77
Putra Erlangga <ryzenrfor@gmail.com>
2026-09-11 18:17:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -611,12 +611,14 @@
       return true;
     });
     
-    terlihat.sort(function (a, b) {
-      if (a.completed !== b.completed) return a.completed ? 1 : -1;
-      var da = cariDeadline(a.mapel); var db = cariDeadline(b.mapel);
-      var va = da.ada ? da.selisihMenit : Infinity; var vb = db.ada ? db.selisihMenit : Infinity;
-      return va - vb;
+    var terlihat = tugasList.filter(function (t) {
+      if (filterAktif === "aktif" && t.completed) return false;
+      if (filterAktif === "selesai" && !t.completed) return false;
+      if (filterMapel !== "semua" && normalisasi(t.mapel) !== normalisasi(filterMapel)) return false;
+      if (cariTugas && (t.mapel + " " + t.detail).toLowerCase().indexOf(cariTugas) === -1) return false;
+      return true;
     });
+    
 
     el.daftarTugas.innerHTML = "";
     terlihat.forEach(function (t) { frag.appendChild(buatItemTugas(t)); });
