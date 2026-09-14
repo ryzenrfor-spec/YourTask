5620de1214c608b9c67d4cf985c9d815835efd24
Putra Erlangga <ryzenrfor@gmail.com>
2026-09-14 10:05:42 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* YourTask Service Worker - GitHub Pages Project Site (v15 - change AI API key) */
-const CACHE_NAME = 'yourtask-cache-v15';
+/* YourTask Service Worker - GitHub Pages Project Site (v16 - change AI API key) */
+const CACHE_NAME = 'yourtask-cache-v16';
 const BASE = '/YourTask/';
 
 const PRECACHE_URLS = [
